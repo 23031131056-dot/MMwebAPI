0f2fe19b2dcac4aebdfa027b199fdb5d84010e5f
23031131056-dot <23031131056@hsncu.edu.in>
2026-02-08 16:13:41 +0530

    Create AlertGeneratorService.java with alert methods

---
+++ src/main/java/com/NirajCS/MoneyManager/service/AlertGeneratorService.java
@@ -0,0 +1,35 @@
+package com.NirajCS.MoneyManager.service;
+
+public class AlertGeneratorService {
+
+    // Method to generate alerts
+    public void generateAlerts() {
+        // Implementation here
+    }
+
+    // Method to generate budget alerts
+    public void generateBudgetAlerts() {
+        // Implementation here
+    }
+
+    // Method to generate high spending alerts
+    public void generateHighSpendingAlerts() {
+        // Implementation here
+    }
+
+    // Method to generate spiking alerts
+    public void generateSpikingAlerts() {
+        // Implementation here
+    }
+
+    // Method to dismiss alerts
+    public void dismissAlert() {
+        // Implementation here
+    }
+
+    // Recommendation method
+    public String getRecommendation() {
+        // Implementation here
+        return "";
+    }
+}
