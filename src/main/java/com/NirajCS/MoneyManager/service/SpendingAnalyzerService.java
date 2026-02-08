ba8efb8324e3528adc8eddf0974d066f08f23ae9
23031131056-dot <23031131056@hsncu.edu.in>
2026-02-08 16:13:29 +0530

    Create SpendingAnalyzerService.java in the service package

---
+++ src/main/java/com/NirajCS/MoneyManager/service/SpendingAnalyzerService.java
@@ -0,0 +1,25 @@
+package com.NirajCS.MoneyManager.service;
+
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SpendingAnalyzerService {
+    
+    public void analyzeCurrentMonth() {
+        // Implementation for analyzing current month's spending
+    }
+
+    public void generateAnalysis() {
+        // Implementation for generating analysis report
+    }
+
+    public void calculateExpenseTrends() {
+        // Implementation for calculating expense trends over time
+    }
+
+    public void compareSpending() {
+        // Implementation for comparing spending between months
+    }
+}
